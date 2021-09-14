d94a08683bb2285c93586dab571e20d28ab1129c
a87920854 <51509697+a87920854@users.noreply.github.com>
2021-09-14 14:23:18 +0800

    navlist

--- include/_bottom.php
+++ include/_bottom.php
@@ -8,18 +8,8 @@
 </html>
 
 <div class="system_pageload">頁面載入時間 0.375 毫秒</div>
-
 <!-- JAVASCRIPT FILES -->
-<script type="text/javascript">
-	var plugin_path = 'assets/plugins/';
-</script>
-<?php //顯示路徑
-$pattern = '/^\/|(php)$/';
-$string = $_SERVER['PHP_SELF'];
-$href = preg_replace($pattern, '', $string);
-echo "<script type='text/javascript'> \$nowmtu = '" . $href . "';</script>";
-?>
-
+<script type="text/javascript">var plugin_path = 'assets/plugins/';</script>
 <script type="text/javascript" src="assets/plugins/jquery/jquery-2.2.3.min.js"></script>
 <script type="text/javascript" src="assets/js/app.js"></script>
 <script type="text/javascript" src="js/util.js"></script>
