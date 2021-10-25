69aa75d9ec005639373eab8785a4cc9319516a28
a87920854 <51509697+a87920854@users.noreply.github.com>
2021-10-25 10:15:41 +0800

    jack_v1.02

---
+++ ad_guest.php
@@ -0,0 +1,145 @@
+<?php
+require_once("_inc.php");
+require_once("./include/_function.php");
+require_once("./include/_top.php");
+require_once("./include/_sidebar.php");
+?>
+
+<!-- MIDDLE -->
+<section id="middle">
+    <!-- page title -->
+    <header id="page-header">
+        <ol class="breadcrumb">
+            <li><a href="index.php">管理系統</a></li>
+            <li class="active">客服中心</li>
+        </ol>
+    </header>
+    <!-- /page title -->
+
+    <div id="content" class="padding-20">
+        <!-- content starts -->
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <span class="title elipsis">
+                    <strong>客服中心　未處理 - 數量：86</strong> <!-- panel title -->
+                </span>
+            </div>
+
+            <div class="panel-body">
+
+                <div class="col-md-12 padding-bottom-10">
+                    <div class="btn-group margin-right-10">
+                        <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">功能 <span class="caret"></span></button>
+                        <ul class="dropdown-menu">
+
+                            <li><a href="?s99=1" target="_self"><i class="icon-resize-horizontal"></i> 切換已處理</a></li>
+
+                        </ul>
+                    </div>
+
+                    <a class="btn btn-info" href="?c=0" disabled>春天/DMN 客服中心 (86)</a>
+                    &nbsp;&nbsp;<a class="btn btn-info" href="?c=1">約專客服中心 (0)</a>
+                    &nbsp;&nbsp;<a class="btn btn-info" href="?c=2">約專會員檢舉 (0)</a>
+                    &nbsp;&nbsp;<a class="btn btn-info" href="ad_custom_complaint.php">客戶申訴</a>
+                </div>
+
+                <table class="table table-striped table-bordered bootstrap-datatable">
+
+                    <thead>
+                        <tr>
+                            <th>姓名</th>
+                            <th>性別</th>
+                            <th>年次</th>
+                            <th>學歷</th>
+                            <th>地區</th>
+                            <th>手機</th>
+                            <th>E-mail</th>
+                            <th>留言內容</th>
+                            <th>會館</th>
+                            <th></th>
+                        </tr>
+
+                        <tr>
+                            <td class="center">江琳是渾蛋虛偽死騙子</td>
+                            <td class="center">男</td>
+                            <td class="center"></td>
+                            <td class="center"></td>
+                            <td class="center">新北市</td>
+                            <td class="center">0919576903</td>
+                            <td class="center">my_love_to_apple@yahoo.com.tw</td>
+                            <td style="word-break: break-all;min-width:300px;">桃園騙子會館<br>合約的時間用騙的<br>排約對象也用騙的<br>從開始就在說謊<br>到最後一次排約<br>用了簽約當天問的<br>1.不能生育的能接受嗎---不能<br>2.離婚的能接受嗎--------不能<br>最後一次<br>問了這樣條件的兩個人後就直接把電話掛了<br>並沒有告知後續有無對象安排<br>只發了簡訊告知有排約<br>惡劣啊<br>跟第一個排約對象一樣啊<br>彭姐：這次只是先試試，我知道你不喜歡，但還是請你來試試(第一次叫試試)<br>試個屁啊那次直接變成合約開始的時間(江琳這混蛋騙子根本就騙人啊)<br>從一開始就騙我合約時間<br>最後又用欺瞞的介紹對象<br>更不用說<br>問我有什麼條件<br>開給你們~說沒問題<br>結果還真的都做到<br>100%做到，零失誤啊<br>不要的全做到了<br>江琳是渾蛋虛偽死騙子從頭騙到尾<br>向他反應<br>僅僅只回復&quot;這是誤會&quot;<br>可惡的死騙子<br>反正我最後也沒佔到你們貴公司任何便宜<br>而你們居然用騙的<br>我他媽的在母親節被騙去關在精神病院一小時<br>然後一句道歉的話都沒有收到過<br>僅僅是誤會<br>到死都不會原諒這兩個死騙子<br>尤其是<br>江琳這個混蛋虛偽死騙子<br>怒不可遏<br><br>
+                                <font color="#FF0000" size="2">處理情形：
+                                </font><br>2021/10/24 上午 11:20:28
+                            </td>
+                            <td class="center">桃園</td>
+                            <td width=80 class="center">
+                                <div class="btn-group">
+                                    <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">操作 <span class="caret"></span></button>
+                                    <ul class="dropdown-menu pull-right">
+
+                                        <li><a href="javascript:Mars_popup('ad_guard_send_branch.php?g_auto=17401','','scrollbars=yes,status=yes,menubar=yes,resizable=yes,width=400,height=250,top=100,left=100');"><i class="icon-arrow-right"></i> 發送</a></li>
+                                        <li><a href="javascript:Mars_popup2('ad_guest_del.php?g_auto=17401','','status=yes,menubar=yes,resizable=yes,scrollbars=yes,width=300,height=200,top=150,left=150');"><i class="icon-trash"></i> 刪除</a></li>
+
+                                        <li><a href="javascript:Mars_popup('ad_guest_fix.php?g_auto=17401','','status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=500,height=320,top=100,left=100');"><i class="icon-pencil"></i> 處理</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td class="center">羅佩瑜</td>
+                            <td class="center">女</td>
+                            <td class="center"></td>
+                            <td class="center"></td>
+                            <td class="center">台中市</td>
+                            <td class="center">0928001050</td>
+                            <td class="center">shadowstranger72@yahoo.com.tw</td>
+                            <td style="word-break: break-all;min-width:300px;">我遇到消費糾紛感受不好<br>請盡速與我聯繫<br>謝謝您<br>
+                                <font color="#FF0000" size="2">處理情形：
+                                </font><br>2021/9/22 下午 12:54:43
+                            </td>
+                            <td class="center">台中</td>
+                            <td width=80 class="center">
+                                <div class="btn-group">
+                                    <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">操作 <span class="caret"></span></button>
+                                    <ul class="dropdown-menu pull-right">
+
+                                        <li><a href="javascript:Mars_popup('ad_guard_send_branch.php?g_auto=17371','','scrollbars=yes,status=yes,menubar=yes,resizable=yes,width=400,height=250,top=100,left=100');"><i class="icon-arrow-right"></i> 發送</a></li>
+                                        <li><a href="javascript:Mars_popup2('ad_guest_del.php?g_auto=17371','','status=yes,menubar=yes,resizable=yes,scrollbars=yes,width=300,height=200,top=150,left=150');"><i class="icon-trash"></i> 刪除</a></li>
+
+                                        <li><a href="javascript:Mars_popup('ad_guest_fix.php?g_auto=17371','','status=yes,menubar=yes,scrollbars=yes,resizable=yes,width=500,height=320,top=100,left=100');"><i class="icon-pencil"></i> 處理</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                </table>
+            </div>
+            <div class="text-center">共 86 筆、第 1 頁／共 2 頁&nbsp;&nbsp;
+                <ul class='pagination pagination-md'>
+                    <li><a href=/ad_guest.php?topage=1>第一頁</a></li>
+                    <li class='active'><a href="#">1</a></li>
+                    <li><a href=/ad_guest.php?topage=2 class='text'>2</a></li>
+                    <li><a href=/ad_guest.php?topage=2 class='text' title='Next'>下一頁</a></li>
+                    <li><a href=/ad_guest.php?topage=2 class='text'>最後一頁</a></li>
+                    <li><select style="width:60px;height:34px;margin-left:5px;" onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
+                            <option value="/ad_guest.php?topage=1" selected>1</option>
+                            <option value="/ad_guest.php?topage=2">2</option>
+                        </select></li>
+                </ul>
+            </div>
+
+        </div>
+        <!--/span-->
+
+    </div>
+    <!--/row-->
+    </div>
+    <!--/.fluid-container-->
+</section>
+<!-- /MIDDLE -->
+
+<?php
+require_once("./include/_bottom.php")
+?>
